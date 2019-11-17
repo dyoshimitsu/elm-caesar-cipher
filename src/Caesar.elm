958aac9a8aa85e21e372cab0d901f23f02f71b8e
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-11-17 17:15:19 +0900

    add table

--- src/Caesar.elm
+++ src/Caesar.elm
@@ -1,4 +1,4 @@
-module Caesar exposing (encode, int2let, let2int, shift)
+module Caesar exposing (encode, int2let, let2int, shift, table)
 
 
 let2int : Char -> Int
@@ -24,3 +24,8 @@ shift n c =
 encode : Int -> String -> String
 encode n xs =
     String.fromList <| List.map (shift n) <| String.toList xs
+
+
+table : List Float
+table =
+    [ 8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0, 6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1 ]
