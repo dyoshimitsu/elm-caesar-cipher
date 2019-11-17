53d19f609c174029cd3b74f815c1ae9cd03b5c36
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-11-17 12:28:09 +0900

    add int2let

--- src/Caesar.elm
+++ src/Caesar.elm
@@ -1,6 +1,11 @@
-module Caesar exposing (let2int)
+module Caesar exposing (int2let, let2int)
 
 
 let2int : Char -> Int
 let2int c =
     Char.toCode c - Char.toCode 'a'
+
+
+int2let : Int -> Char
+int2let n =
+    Char.fromCode (Char.toCode 'a' + n)
